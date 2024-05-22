3e1004205a5ca88fe38e5e4592dee05f1541aea3
Gustavo Vieira <164119202+GustavoJVvieira@users.noreply.github.com>
2024-05-21 22:16:00 -0300

    Update typeorm.migrations-config.ts

--- deus/src/db/typeorm.migrations-config.ts
+++ deus/src/db/typeorm.migrations-config.ts
@@ -12,7 +12,12 @@ const configService = new ConfigService();
 
 const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
-    url: "postgres://bomsjbhmgrsimv:946248d01eae7f37f6478cc7304001fa77164ca84b96300b464a712491eeeda7@ec2-44-194-102-142.compute-1.amazonaws.com:5432/d9gemefgkthods",
+   type: 'postgres',
+        host : 'localhost',
+        username: "postgres",
+        port:5432,
+        password:"admin" ,
+        database:"feedup_teste2" , 
     entities: [UserEntity, FeedbackEntity, MoodsEntity, CommentsEntity],
     migrations: [__dirname + '/migrations/*.ts'],
     synchronize: false
